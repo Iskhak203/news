394f94541f195c02513afead7d4a2322cde56b84
Iskhak <ishakishakov00@gmail.com>
2025-03-27 12:20:43 +0600

    first conmmit

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<script setup>
+
+</script>
+
+<template>
+ <div class="con">
+  <nav class="navbar bg-white border-bottom border-body" data-bs-theme="white">
+    <div> <h1>Iskhak</h1></div>
+    <ul class="nav justify-content-end">
+  <li class="nav-item">
+    <a class="nav-link active" aria-current="page" href="#">Главный</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">О сбе</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Картинка</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link disabled" aria-disabled="true">Контакт</a>
+  </li>
+</ul>
+<form class="d-flex" role="search">
+        <input class="form-control" type="search" placeholder="Поиск">
+        <button class="btn btn-outline-success" type="submit">Издоо</button>
+      </form>
+    </nav>
+    <div class="section">
+      <div>
+        <img src="/img/Iskhak.jpg" alt="">
+      </div>
+      <div>
+        <h2>Алишер уулу Исхак</h2>
+      </div>
+    </div>
+</div>
+</template>
+
+<style scoped>
+.con{
+  margin: auto;
+  width: 97%;
+}
+</style>
